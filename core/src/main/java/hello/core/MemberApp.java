6785dbe9c740cddb9b5bf64b5f392b2f08d15335
지승렬 <seungryeol156@gmail.com>
2022-12-21 15:03:43 +0900

    자바 코드로만 회원 가입 로직 구현

---
+++ core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.core;
+
+import hello.core.memeber.Grade;
+import hello.core.memeber.Member;
+import hello.core.memeber.MemberService;
+import hello.core.memeber.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("newMember = "+member.getName());
+        System.out.println("findMember = "+findMember.getName());
+    }
+}
