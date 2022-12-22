f451a78af07eeafd9842b75f15b6c833c39edc9e
지승렬 <seungryeol156@gmail.com>
2022-12-22 17:47:35 +0900

    AppConfig 수정, 의존관계 명확화와 코드 수정 용이화

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.memeber.MemberService;
 import hello.core.memeber.MemberServiceImpl;
 import hello.core.memeber.MemoryMemberRepository;
@@ -12,10 +14,19 @@ public class AppConfig {
     // 이제 MemberServiceImpl는 인터페이스에만 의존 => DIP를 지킴!
     // => 이걸 생성자 주입이라 함
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
