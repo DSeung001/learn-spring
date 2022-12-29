48f8ca04e5ab9a6e278a658eed38c5b5fa4ad6d9
지승렬 <seungryeol156@gmail.com>
2022-12-29 11:18:45 +0900

    스프링 빈 조회 기본

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,51 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.memeber.MemberService;
+
+import hello.core.memeber.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+
+    // 빈으로 구조와 이어준 구현체로도 체그 가능하다
+    // 아래 코드는 역할이 아닌 구현에 의존하는 코드이다, 하지만 만약을 위해
+    @Test
+    @DisplayName("구현체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 못함")
+    void findBeanByNameX(){
+        MemberService xxxxx = ac.getBean("xxxxx", MemberService.class);
+        // 해당 로직 실행 중 에러가 터져야 한다
+        assertThrows(NoSuchBeanDefinitionException.class, () ->
+                ac.getBean("xxxxx", MemberService.class)
+        );
+    }
+}
