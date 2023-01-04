1aa29d8725b416064ded36fb5d9897ad3152d616
지승렬 <seungryeol156@gmail.com>
2023-01-04 15:51:40 +0900

    스프링 입문 강의 코드

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,13 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,4 +22,8 @@ import org.springframework.context.annotation.FilterType;
 ) // 수동으로 빈을 등록한 AppConfig 제외, + 다른 configuration 들도 제외
 public class AutoAppConfig {
 
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,28 +10,38 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService{
 
     // final로 불변처리, private에는 의존관계 주입 가능
-    @Autowired private MemberRepository memberRepository;
-    @Autowired private DiscountPolicy discountPolicy;
+    // 얘네는 DI 프레임워크가 필수로 사용되어야 함
+    // 사용해도 되는 곳 => 애플리케이션과 상관 없는 테스트 코드
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
 
-  /*  @Autowired(required = false)
-    public void setMemberRepository(MemberRepository memberRepository) {
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    /*public void setMemberRepository(MemberRepository memberRepository) {
         System.out.println("memberRepository = " + memberRepository);
         this.memberRepository = memberRepository;
     }
 
-    @Autowired
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         System.out.println("discountPolicy = " + discountPolicy);
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
     // 생성자가 하나일 때는 자동으로 Autowired를 해줘서 생략이 가능
-    @Autowired
+//    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }*/
+    }
+
+//    일반 메서드 주입, 한번에 여러 필드를 주입 받을 수 있다 => 보통 생성자로 대체함
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
