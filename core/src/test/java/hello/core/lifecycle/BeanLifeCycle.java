0aa510434c95b0dfd47d20fd04f5ff20d491ea74
지승렬 <seungryeol156@gmail.com>
2023-01-09 15:43:21 +0900

    빈 생명주기 콜백 시작

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycle.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycle.java
@@ -1,2 +1,28 @@
-package hello.core.lifecycle;public class BeanLifeCycle {
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycle {
+    @Test
+    public void lifeCycleTest() {
+        // 상위이기에 매핑 가능
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            // 외부에서 초기화를 하고 호출해야하는 경우가 존재함
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
 }
