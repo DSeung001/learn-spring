a4487343118fa7f1fdac9a9e3166f7ec2fe209b2
지승렬 <seungryeol156@gmail.com>
2023-01-11 16:46:48 +0900

    스코프와 프록시'

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -16,21 +16,23 @@ public class LogDemoController
 {
     // 자동 주입
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final  MyLogger myLogger;
 
     // 뷰 없이 localhost:포트/log-demo
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-        // DL을 통해 Request가 오면 실행하게 할 있음
-        MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger = " + myLogger.getClass());
+        // 결과 값이 myLogger = class hello.core.common.MyLogger$$EnhancerBySpringCGLIB$$700597e
+        // SpringCGLIB => 뭔가 스프링이 조작하는 느낌
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
         Thread.sleep(2000);
         // request scope이므로 아래에서도 실행!
         logDemoService.logic("testId");
+        System.out.println("myLogger = " + myLogger.getClass());
         return "OK";
     }
 }
