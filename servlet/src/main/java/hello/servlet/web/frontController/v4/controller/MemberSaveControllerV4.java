6536167c2fe86eff614a01ed3a6f070bd8b0f507
지승렬 <seungryeol156@gmail.com>
2023-01-16 13:58:43 +0900

    유연한 컨트롤러2 - v5

--- servlet/src/main/java/hello/servlet/web/frontController/v4/controller/MemberSaveControllerV4.java
+++ servlet/src/main/java/hello/servlet/web/frontController/v4/controller/MemberSaveControllerV4.java
@@ -18,7 +18,7 @@ public class MemberSaveControllerV4 implements ControllerV4 {
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        model.put("member", model);
+        model.put("member", member);
         return "save-result";
     }
 }
