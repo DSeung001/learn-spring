8f27f1d0cf2a70afa62bb4508e64188788ab13e2
지승렬 <seungryeol156@gmail.com>
2023-01-16 15:14:42 +0900

    뷰 리졸버

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 // 하위 경로의 모든 서블릿을 탐색하여 자동 등록
 @ServletComponentScan
@@ -10,8 +13,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 public class ServletApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServletApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServletApplication.class, args);
+    }
 }
