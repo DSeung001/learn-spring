3d1f7298150787895d066deec5c9c4041260b1d3
seungryeol <seungryeol156@gmail.com>
2023-01-23 11:33:22 +0900

    타임리프

---
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,41 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    // 생성자 주입으로 Autowired 할 수 있지만, 생성자가 하나면 생략이 가능하다
+    // 근데 또 RequiredArgsConstructor를 사용함으로써 final 붙은 값에 대한 생성자 생략이 가능
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items",items);
+        return "basic/items";
+    }
+
+    // 테스트 용
+    // 객체가 생성된 후 별도의 초기화 작업으로 이 메소드를 진행해줌
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
