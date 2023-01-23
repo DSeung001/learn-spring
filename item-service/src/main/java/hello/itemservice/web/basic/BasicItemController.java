966c821c0d0666fdbd8258c9a40ac63fa201da19
seungryeol <seungryeol156@gmail.com>
2023-01-23 12:24:30 +0900

    상품 등록 처리-@ModelAttribute

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,11 +5,9 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -45,9 +43,49 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+//    @PostMapping("/add")
+    public String saveItemV1(
+            @RequestParam String itemName,
+            @RequestParam int price,
+            @RequestParam Integer quantity,
+            Model model
+    ){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    // @ModelAttribute는 자동 Setter 호출과 모델에 매핑도 해줌!
+//    @PostMapping("/add")
+    public String saveItemV2(
+            @ModelAttribute("item") Item item,
+            Model model
+    ){
+        itemRepository.save(item);
+//        model.addAttribute("item",item); 자동으로 추가 해준다
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String saveItemV2(
+            @ModelAttribute Item item
+    ){
+        // ModelAttribute에 alias를 지정안하면 클래스의 첫글자를 바꾼 값으로 모델에 넣어줌
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    // String이나 int 같은 단순 타입이 오면 RequestParam이 적용되고 아닌 경우 객체를 통해 받음
+    // 아래 처럼 ModelAttribute도 생략이 가능, 마찬가지로 모델에는 클래스 명의 첫글자가 소문자로 들어감
     @PostMapping("/add")
-    public String save(){
-        return "basic/addForm";
+    public String saveItemV3(Item item){
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     // 테스트 용
