87c109b5cd07ff105ec59cc9fbe1d9c8b0e055bc
seungryeol <seungryeol156@gmail.com>
2023-01-23 11:59:45 +0900

    상품 등록 폼

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -32,18 +33,28 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addFrom(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(){
+        return "basic/addForm";
+    }
+
     // 테스트 용
     // 객체가 생성된 후 별도의 초기화 작업으로 이 메소드를 진행해줌
     @PostConstruct
     public void init(){
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
-    }
-
-    @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId, Model model){
-        Item item = itemRepository.findById(itemId);
-        model.addAttribute("item",item);
-        return "basic/item";
     }
 }
