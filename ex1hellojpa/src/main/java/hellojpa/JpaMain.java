70e8bb0ee2a5ef25cf2ba2d57bbb9833f464196c
지승렬 <seungryeol156@gmail.com>
2023-01-27 14:20:15 +0900

    임시 저장

--- ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -8,6 +8,42 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 비영속 상태
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            // 영속 상태, EntityManager 안에 영속성 컨텍스트에서 관리됨 => 바로 쿼리가 실행되지 않음
+            // BEFORE & AFTER 후에 쿼리가 로그에 찍힘
+            System.out.println("=========BEFORE=========");
+            em.persist(member);
+//            em.detach(member); // 영속성 컨텍스트에서 분리 => 준영속 상태 => 쿼리 실행을 안함
+            System.out.println("=========AFTER=========");
+
+            // insert 한 값이 1차 캐시에 저장되서 별도의 select 쿼리가 실행안함
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+
+            // Transaction을 commit시 쿼리 실행
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    /*
+    단원 : JPA 소개
+    public static void main(String[] args) {
         // EntityManagerFactory는 WAS에 올라오는 시점에 하나만 생겨서 공유
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         // EntityManager는 한번 쓰고 버리는 것, 쓰레드간에 공유하면 안됨 (DB 커넥션)
@@ -48,5 +84,5 @@ public class JpaMain {
             em.close();
         }
         emf.close();
-    }
+    }*/
 }
