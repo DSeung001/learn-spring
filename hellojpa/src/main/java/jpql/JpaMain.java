84c6ea958c2876046fe5fcab93e373ac032ecd4f
seungryeol <seungryeol156@gmail.com>
2023-02-18 11:17:19 +0900

    조인

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -5,8 +5,53 @@ import java.util.List;
 import java.util.Objects;
 
 public class JpaMain {
-    // 페이징 API
+    // 조인
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("teamA");
+            member.setAge(10);
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+//            String query = "select m from Member m inner join m.team t";
+            // left outer join에서 outer는 생략 가능
+//            String query = "select m from Member m left outer join m.team t";
+//            String query = "select m from Member m, Team t where m.username = t.name";
+
+            // On 추가
+//            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
+            // 막조인, 연관관계는 빼고도 조인 가능
+            String query = "select m from Member m left join Team t on m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+            System.out.println("result.size() = " + result.size());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    // 페이징 API
+    /*public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -40,7 +85,7 @@ public class JpaMain {
             em.close();
         }
         emf.close();
-    }
+    }*/
 
     // 프로젝트
     /*public static void main(String[] args) {
