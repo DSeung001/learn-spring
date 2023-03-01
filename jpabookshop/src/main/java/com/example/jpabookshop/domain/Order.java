aaab0c1cdc7434d0f3bb8c1685f149860b9f408a
seungryeol <seungryeol156@gmail.com>
2023-03-01 12:00:46 +0900

    엔티티 설계시 주의점

--- jpabookshop/src/main/java/com/example/jpabookshop/domain/Order.java
+++ jpabookshop/src/main/java/com/example/jpabookshop/domain/Order.java
@@ -19,14 +19,15 @@ public class Order {
     private Long id;
 
     // 연관관계의 주인, 이걸 수정하면 member의 키도 바뀜
-    @ManyToOne
+    @ManyToOne(fetch =  FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    // Order에다가 OrderItem을 넣고 저장하면 같이 저장된다
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch =  FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,4 +35,24 @@ public class Order {
 
     @Enumerated(EnumType.ORDINAL)
     private OrderStatus status;
+
+    // 연관관계 편의 메서드
+    // Many to One
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    // One to Many
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    // 양방향에서는 하는 게 좋음
+    // One to One
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
