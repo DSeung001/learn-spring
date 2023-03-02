960c65c851efdb63fc4bae491d41cb242ada6319
seungryeol <seungryeol156@gmail.com>
2023-03-02 22:19:02 +0900

    회원 리포지토리 개발

---
+++ jpabookshop/src/main/java/com/example/jpabookshop/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package com.example.jpabookshop.repository;
+
+import com.example.jpabookshop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+// Repository => Component 가 안에 있어 스프링에서 스캔해감
+@Repository
+public class MemberRepository {
+
+    // 자동 주입
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name :=name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
