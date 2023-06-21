913a03ddef8da0ffd70061159d0da7af8bc3f3d8
지승렬 <hsnc23032108@hanwha.com>
2023-06-21 14:06:03 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- jpabookshop/src/main/java/com/example/jpabookshop/domain/item/Item.java
+++ jpabookshop/src/main/java/com/example/jpabookshop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.jpabookshop.domain.item;
 
 import com.example.jpabookshop.domain.Category;
+import com.example.jpabookshop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * Stock 증가
+     * @param quantity
+     */
+    /* 비즈니스 로직 */
+    /* 도메인 주도 개발 시 엔티티안에 비즈니스 로직을 만듦 */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("ned more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
